edbe7ae55df229c134268ad1ad3ddbcb13a77af6
Wokgui <149906755+Wokgui@users.noreply.github.com>
2026-08-11 23:25:22 +0200

    Force le rafraîchissement du correctif des icônes

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const STATIC_CACHE="copenhague-v330-static-v1";
-const RUNTIME_CACHE="copenhague-v330-runtime-v1";
+const STATIC_CACHE="copenhague-v331-static-v1";
+const RUNTIME_CACHE="copenhague-v331-runtime-v1";
 const STATIC_FILES=[
   "/",
   "/index.html",
