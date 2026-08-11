cf8e419d6ef58f301025d01e459e80839e99b364
Wokgui <149906755+Wokgui@users.noreply.github.com>
2026-08-11 20:52:21 +0200

    Bump PWA cache for Suivi icon fix

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const STATIC_CACHE="copenhague-v327-static-v1";
-const RUNTIME_CACHE="copenhague-v327-runtime-v1";
+const STATIC_CACHE="copenhague-v328-static-v1";
+const RUNTIME_CACHE="copenhague-v328-runtime-v1";
 const STATIC_FILES=[
   "/",
   "/index.html",
