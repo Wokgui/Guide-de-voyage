45136f448d3c35407e55ae295b7bfca2987e1d7c
Wokgui <149906755+Wokgui@users.noreply.github.com>
2026-08-11 12:52:07 +0200

    Forcer le cache PWA v316 final

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const STATIC_CACHE="copenhague-v315-static-v1";
-const RUNTIME_CACHE="copenhague-v315-runtime-v1";
+const STATIC_CACHE="copenhague-v316-static-v1";
+const RUNTIME_CACHE="copenhague-v316-runtime-v1";
 const STATIC_FILES=[
   "/",
   "/index.html",
