f15b35a3eb13758c02a7c9fb9a89faf7513aeb13
Wokgui <149906755+Wokgui@users.noreply.github.com>
2026-08-11 14:38:16 +0200

    Forcer cache v319 pour badge et navigation Maps

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const STATIC_CACHE="copenhague-v318-static-v1";
-const RUNTIME_CACHE="copenhague-v318-runtime-v1";
+const STATIC_CACHE="copenhague-v319-static-v1";
+const RUNTIME_CACHE="copenhague-v319-runtime-v1";
 const STATIC_FILES=[
   "/",
   "/index.html",
