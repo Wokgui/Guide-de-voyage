0af12b6fb9f8abdb7802a24335e998d4e2e3583b
Wokgui <149906755+Wokgui@users.noreply.github.com>
2026-08-12 09:18:26 +0200

    feat: aligner les fiches de la carte

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const STATIC_CACHE="copenhague-v326-static-v1";
-const RUNTIME_CACHE="copenhague-v326-runtime-v1";
+const STATIC_CACHE="copenhague-v327-static-v1";
+const RUNTIME_CACHE="copenhague-v327-runtime-v1";
 const STATIC_FILES=[
   "/",
   "/index.html",
