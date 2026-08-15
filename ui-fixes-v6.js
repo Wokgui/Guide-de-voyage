2c8feb22ab42364ae2510fbe3d88b4dd1e3aae77
Wokgui <149906755+Wokgui@users.noreply.github.com>
2026-08-15 08:53:54 +0200

    Supprimer ancien patch UI non chargé

--- ui-fixes-v6.js
+++
@@ -1,9 +0,0 @@
-(function(){
-"use strict";
-if(document.querySelector('script[data-cph-ui-fixes-v9="1"]'))return;
-const s=document.createElement("script");
-s.src="/ui-fixes-v7.js?v=9";
-s.async=false;
-s.dataset.cphUiFixesV9="1";
-document.head.appendChild(s);
-})();
